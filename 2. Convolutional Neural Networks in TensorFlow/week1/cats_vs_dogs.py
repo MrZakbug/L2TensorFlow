b30a84ad21587285c29e9ded97764154ef7262e0
MrZakbug <MrZakbug@gmail.com>
2019-08-01 23:12:06 +0200

    cats_vs_dogs simple model

---
+++ 2. Convolutional Neural Networks in TensorFlow/week1/cats_vs_dogs.py
@@ -0,0 +1,73 @@
+# %%
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.optimizers import RMSprop
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from keras_preprocessing import image
+print(tf.__version__)
+tf.test.is_gpu_available() and tf.test.is_built_with_cuda()
+
+# %%
+model = tf.keras.models.Sequential([
+    tf.keras.layers.Conv2D(16, (3, 3), activation='relu',
+                           input_shape=(150, 150, 3)),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(32, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Conv2D(64, (3, 3), activation='relu'),
+    tf.keras.layers.MaxPooling2D(2, 2),
+    tf.keras.layers.Flatten(),
+    tf.keras.layers.Dense(512, activation='relu'),
+    tf.keras.layers.Dense(1, activation='sigmoid')
+])
+#%%
+model.summary()
+
+# %%
+model.compile(optimizer=RMSprop(lr=0.001),
+              loss='binary_crossentropy',
+              metrics=['acc'])
+
+# %%
+train_dir = os.getcwd() + '/support-files/cats_and_dogs_filtered/train'
+validation_dir = os.getcwd() + '/support-files/cats_and_dogs_filtered/validation'
+
+train_datagen = ImageDataGenerator(rescale=1.0/255.)
+test_datagen = ImageDataGenerator(rescale=1.0/255.)
+
+train_generator = train_datagen.flow_from_directory(train_dir,
+                                                    batch_size=20,
+                                                    class_mode='binary',
+                                                    target_size=(150, 150))
+
+validation_generator =  test_datagen.flow_from_directory(validation_dir,
+                                                         batch_size=20,
+                                                         class_mode='binary',
+                                                         target_size=(150, 150))
+
+
+# %%
+history = model.fit_generator(train_generator,
+                              validation_data=validation_generator,
+                              steps_per_epoch=100,
+                              epochs=15,
+                              validation_steps=50,
+                              verbose=2)
+
+# %%
+downloaded_files = '../../../Downloads/'
+img = image.load_img(downloaded_files+'dog1.jpg', target_size=(150, 150))
+
+x = image.img_to_array(img)
+x = np.expand_dims(x, axis=0)
+images = np.vstack([x])
+
+classes = model.predict(images, batch_size=10)
+
+print(classes[0])
+
+if classes[0] > 0:
+    print('It is a dog!')
+else:
+    print('It is a cat...')
